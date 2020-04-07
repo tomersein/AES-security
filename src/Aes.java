dba19924b702bd20b7b4b58f93e8ae2068ffb2e5
Itai Katz <itaikatz7@gmail.com>
2020-04-07 15:55:47 +0300

    Encryption code

--- src/Aes.java
+++ src/Aes.java
@@ -7,13 +7,100 @@ public class Aes {
 
     /**
      * this function gets a path, reads a file and returns an array of bytes
+     *
      * @param path to the file
      * @return byte array
      * @throws IOException
      */
-    public byte [] readKey (String path) throws IOException {
+    public byte[] readKey(String path) throws IOException {
+
         Path filePath = Paths.get(path);
-        byte [] arrByte = Files.readAllBytes(filePath);
+        byte[] arrByte = Files.readAllBytes(filePath);
         return arrByte;
+    }
+
+    /**
+     * The function receives a message and 3 keys and encrypts the message using the 3 keys
+     *
+     * @param message
+     * @param firstKey
+     * @param secondKey
+     * @param thirdKey
+     * @return blockToEncrypt
+     */
+    public byte[][] encryptMessage(byte[][] message, byte[][] firstKey, byte[][] secondKey, byte[][] thirdKey) {
+
+        byte[][] blockToEncrypt = new byte[4][4];
+        for (int i = 0; i < blockToEncrypt.length; i++) {
+            for (int j = 0; j < blockToEncrypt[0].length; j++) {
+                blockToEncrypt[i][j] = message[i][j];
+            }
+        }
+        //first iteration
+        blockToEncrypt = shiftColumns(blockToEncrypt);
+        blockToEncrypt = addRoundKey(blockToEncrypt, firstKey);
+        //second iteration
+        blockToEncrypt = shiftColumns(blockToEncrypt);
+        blockToEncrypt = addRoundKey(blockToEncrypt, secondKey);
+        //third iteration
+        blockToEncrypt = shiftColumns(blockToEncrypt);
+        blockToEncrypt = addRoundKey(blockToEncrypt, thirdKey);
+
+        return blockToEncrypt;
+    }
+
+    /**
+     * The function shifts the message's columns
+     * @param message
+     * @return shiftedMessage
+     */
+    private byte[][] shiftColumns(byte[][] message) {
+
+        byte[][] shiftedMessage = new byte[4][4];
+        //not touching the first column
+        for (int i = 0; i < 4; i++) {
+            shiftedMessage[i][0] = message[i][0];
+        }
+        //shifting the second column one cell upwards
+        for (int i = 1; i < 4; i++) {
+            shiftedMessage[i-1][1] = message[i][1];
+        }
+        shiftedMessage[3][1] = message[0][1];
+        //shifting the third column two cells upwards
+        shiftedMessage[0][2] = message[2][2];
+        shiftedMessage[1][2] = message[3][2];
+        shiftedMessage[2][2] = message[0][2];
+        shiftedMessage[3][2] = message[1][2];
+        //shifting the fourth column three cells upwards
+        for(int i=0; i<3; i++){
+            shiftedMessage[i+1][3] = message[i][3];
+        }
+        shiftedMessage[0][3] = message[3][3];
+
+        return shiftedMessage;
+    }
+
+    /**
+     * The function adds the key to the message by using the 'xor' function
+     * @param message
+     * @param key
+     * @return addRoundKey
+     */
+    private byte[][] addRoundKey(byte[][] message, byte[][] key){
+
+        byte[][] roundedMessage = new byte[4][4];
+        int messageBit;
+        int keyBit;
+        int combinedBit;
+
+        for(int i=0; i<4; i++){
+            for(int j=0; j<4; j++){
+                messageBit = (int)message[j][i];
+                keyBit = (int)key[j][i];
+                combinedBit = messageBit^keyBit;
+                roundedMessage[j][i] = (byte)(0xff & combinedBit);
+            }
+        }
+        return roundedMessage;
     }
 }
