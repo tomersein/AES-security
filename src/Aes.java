4c43d3153f5d20dddc4b5302f18f7067785dc92a
Itai Katz <itaikatz7@gmail.com>
2020-04-07 19:34:31 +0300

    Encryption + Decryption code Half of the deciphering code

--- src/Aes.java
+++ src/Aes.java
@@ -7,25 +7,27 @@ public class Aes {
 
     /**
      * this function gets a path, reads a file and returns an array of bytes
+     *
      * @param path to the file
      * @return byte array
      * @throws IOException
      */
-    public byte [] readKey (String path) throws IOException {
+    public byte[] readKey(String path) throws IOException {
         Path filePath = Paths.get(path);
-        byte [] arrByte = Files.readAllBytes(filePath);
+        byte[] arrByte = Files.readAllBytes(filePath);
         return arrByte;
     }
 
     /**
      * this function takes an array and writes it into a file
-     * @param path the path we will write the file to
+     *
+     * @param path    the path we will write the file to
      * @param arrByte the array we will write to a file
      * @throws IOException
      */
-    public void writeKeys (String path, byte [] arrByte) throws IOException {
+    public void writeKeys(String path, byte[] arrByte) throws IOException {
         Path filePath = Paths.get(path);
-        Files.write(filePath,arrByte);
+        Files.write(filePath, arrByte);
     }
 
     /**
@@ -92,9 +94,9 @@ public class Aes {
     }
 
 
-
     /**
      * The function shifts the message's columns
+     *
      * @param message
      * @return shiftedMessage
      */
@@ -107,7 +109,7 @@ public class Aes {
         }
         //shifting the second column one cell upwards
         for (int i = 1; i < 4; i++) {
-            shiftedMessage[i-1][1] = message[i][1];
+            shiftedMessage[i - 1][1] = message[i][1];
         }
         shiftedMessage[3][1] = message[0][1];
         //shifting the third column two cells upwards
@@ -116,15 +118,15 @@ public class Aes {
         shiftedMessage[2][2] = message[0][2];
         shiftedMessage[3][2] = message[1][2];
         //shifting the fourth column three cells upwards
-        for(int i=0; i<3; i++){
-            shiftedMessage[i+1][3] = message[i][3];
+        for (int i = 0; i < 3; i++) {
+            shiftedMessage[i + 1][3] = message[i][3];
         }
         shiftedMessage[0][3] = message[3][3];
 
         return shiftedMessage;
     }
 
-    private byte[][] shiftColumnsBackwards(byte[][] message){
+    private byte[][] shiftColumnsBackwards(byte[][] message) {
 
         byte[][] shiftedMessage = new byte[4][4];
         //not touching the first column
@@ -133,7 +135,7 @@ public class Aes {
         }
         //shifting the second column one cell downwards
         for (int i = 1; i < 4; i++) {
-            shiftedMessage[i][1] = message[i-1][1];
+            shiftedMessage[i][1] = message[i - 1][1];
         }
         shiftedMessage[0][1] = message[3][1];
 
@@ -142,23 +144,24 @@ public class Aes {
 
     /**
      * The function adds the key to the message by using the 'xor' function
+     *
      * @param message
      * @param key
      * @return addRoundKey
      */
-    private byte[][] addRoundKey(byte[][] message, byte[][] key){
+    private byte[][] addRoundKey(byte[][] message, byte[][] key) {
 
         byte[][] roundedMessage = new byte[4][4];
         int messageBit;
         int keyBit;
         int combinedBit;
 
-        for(int i=0; i<4; i++){
-            for(int j=0; j<4; j++){
-                messageBit = (int)message[j][i];
-                keyBit = (int)key[j][i];
-                combinedBit = messageBit^keyBit;
-                roundedMessage[j][i] = (byte)(0xff & combinedBit);
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                messageBit = (int) message[j][i];
+                keyBit = (int) key[j][i];
+                combinedBit = messageBit ^ keyBit;
+                roundedMessage[j][i] = (byte) (0xff & combinedBit);
             }
         }
         return roundedMessage;
@@ -166,16 +169,17 @@ public class Aes {
 
     /**
      * this function takes an array of bytes and converts it into a matrix
+     *
      * @param byteArr
      * @return
      */
-    public byte [][] convertToMatrix (byte [] byteArr){
-        int numOfCol = byteArr.length/4;
-        byte [][] matrix = new byte [4][numOfCol];
+    public byte[][] convertToMatrix(byte[] byteArr) {
+        int numOfCol = byteArr.length / 4;
+        byte[][] matrix = new byte[4][numOfCol];
         int indexOfByteArray = 0;
-        for(int i=0; i<numOfCol;i++){
-            for(int j=0;j<4; j++){
-                matrix[j][i]=byteArr[indexOfByteArray];
+        for (int i = 0; i < numOfCol; i++) {
+            for (int j = 0; j < 4; j++) {
+                matrix[j][i] = byteArr[indexOfByteArray];
                 indexOfByteArray++;
             }
         }
@@ -184,16 +188,17 @@ public class Aes {
 
     /**
      * this function takes a matrix and converts it into an array
+     *
      * @param matrix of bytes
      * @return the array
      */
-    public byte [] convertMatrixToArr (byte [][] matrix){
+    public byte[] convertMatrixToArr(byte[][] matrix) {
         int arrLength = matrix.length * matrix[0].length;
-        byte [] arrByte = new byte [arrLength];
+        byte[] arrByte = new byte[arrLength];
         int counter = 0;
-        for(int i=0; i<matrix[0].length;i++){
-            for(int j=0; j<matrix.length; j++){
-                arrByte[counter]=matrix[j][i];
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                arrByte[counter] = matrix[j][i];
                 counter++;
             }
         }
@@ -201,6 +206,32 @@ public class Aes {
     }
 
 
+    public void breakAesAlgorithm(byte[][] message, byte[][] cipher) {
+
+        byte[][] attackingBlock;
+        byte[][] thirdKey;
+        byte[][] secondKey;
+        byte[][] firstKey;
+
+        //first, randomize two keys that will aid in finding the third key
+        byte[] firstKeyVal = new byte[128];
+        byte[] secondKeyVal = new byte[128];
+        for (int i = 0; i < firstKeyVal.length; i++) {
+            firstKeyVal[i] = (byte)((int)(Math.random()*128));
+            secondKeyVal[i] = (byte)((int)(Math.random()*128));
+        }
+        byte[][] firstRandomKey = convertToMatrix(firstKeyVal);
+        byte[][] secondRandomKey = convertToMatrix(secondKeyVal);
+
+        //now we will generate a new cipher-text using the 2 random
+        attackingBlock = shiftColumns(message);
+        attackingBlock = addRoundKey(message, firstRandomKey);
+        attackingBlock = shiftColumns(attackingBlock);
+        attackingBlock = addRoundKey(message, secondRandomKey);
+
+        //with the two cipher-texts we can now find the actual third key:
+
+    }
 
 }
 
